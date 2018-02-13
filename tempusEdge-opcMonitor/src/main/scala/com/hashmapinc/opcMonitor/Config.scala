4244e9a51fd55b4d6aac033a7438fc77a83fd19b
Randy Pitcher <randypitcherii@gmail.com>
2018-02-13 17:29:07 -0600

    added logging to config

--- tempusEdge-opcMonitor/src/main/scala/com/hashmapinc/opcMonitor/Config.scala
+++ tempusEdge-opcMonitor/src/main/scala/com/hashmapinc/opcMonitor/Config.scala
@@ -1,5 +1,7 @@
 package com.hashmapinc.opcMonitor
 
+import com.typesafe.scalalogging.Logger
+
 object Config {
   // general configs
   var context = "production"
@@ -8,10 +10,15 @@ object Config {
 
   // OPC configs 
 
+  val log = Logger(getClass())
+  log.info("Setting default configs")
+
   /**
    * This function sets the configs to test values
    */
   def initTestContext: Unit = {
+    log.info("Setting test configs")
+    
     // general configs
     context = "test"
 
