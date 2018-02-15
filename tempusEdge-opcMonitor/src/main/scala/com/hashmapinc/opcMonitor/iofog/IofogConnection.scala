d8d6784261f2e0eb096dbb07831bcd9e04244420
Randy Pitcher <randypitcherii@gmail.com>
2018-02-15 12:11:45 -0600

    added iofog connection logic

--- tempusEdge-opcMonitor/src/main/scala/com/hashmapinc/opcMonitor/iofog/IofogConnection.scala
+++ tempusEdge-opcMonitor/src/main/scala/com/hashmapinc/opcMonitor/iofog/IofogConnection.scala
@@ -3,6 +3,8 @@ package com.hashmapinc.opcMonitor.iofog
 import com.iotracks.api.IOFogClient
 import com.typesafe.scalalogging.Logger
 
+import com.hashmapinc.opcMonitor.Config
+
 /**
  * This object creates and manages the iofog connection.
  */
@@ -10,5 +12,17 @@ object IofogConnection {
   val log = Logger(getClass())
   log.info("Creating iofog connection")
 
-  //val client = new IOFogClient()
+  val client = new IOFogClient("", 0, Config.CONTAINER_ID) //use default values for client
+  
+  try {
+    client.openControlWebSocket(IofogController);
+  } catch {
+    case e: Exception => log.error("IoFog websocket error: " + e.toString)
+  }
+
+  try {
+    client.openMessageWebSocket(IofogController);
+  } catch {
+    case e: Exception => log.error("IoFog websocket error: " + e.toString)
+  }
 }
