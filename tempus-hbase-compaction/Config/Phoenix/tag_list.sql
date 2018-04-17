48f58773cfb78ee8d770de9e0301684c94d93380
Sudeep Sunthankar <sudeep.sunthankar@hashmapinc.com>
2018-04-17 15:16:26 +1000

    Updated SQL

--- tempus-hbase-compaction/Config/Phoenix/tag_list.sql
+++ tempus-hbase-compaction/Config/Phoenix/tag_list.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS tag_list;
 CREATE TABLE tag_list(id BIGINT PRIMARY KEY, name VARCHAR(50), uri VARCHAR(150), uuid VARCHAR(50), datatype VARCHAR(16),
-assetid VARCHAR, channelname VARCHAR, channelid BIGINT, source VARCHAR, status SMALLINT, uom VARCHAR(32), startdate DATE, enddate DATE, description VARCHAR(50) ) COMPRESSION='SNAPPY';
+assetid VARCHAR, channelname VARCHAR, channelid BIGINT, source VARCHAR, status SMALLINT, uom VARCHAR(32), startdate DATE, enddate DATE, description VARCHAR(50) ) COMPRESSION='SNAPPY'
