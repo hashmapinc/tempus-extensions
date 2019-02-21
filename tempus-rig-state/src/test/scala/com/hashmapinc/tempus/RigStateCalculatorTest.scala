a48daa75bb98edd2db36483d697a16e864fef343
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2019-02-21 13:43:11 +0530

    Updating rig state unit tests according to new factors

--- tempus-rig-state/src/test/scala/com/hashmapinc/tempus/RigStateCalculatorTest.scala
+++ tempus-rig-state/src/test/scala/com/hashmapinc/tempus/RigStateCalculatorTest.scala
@@ -22,92 +22,92 @@ class RigStateCalculatorTest extends FlatSpec {
   }
 
   "it" should "calculate rig state and return Stationary" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 80, 5, 0, 100)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 0.00630802, 1.00, 0, 3447280)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Stationary")
   }
 
   "it" should "calculate rig state and return Rotating" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 80, 35, 0, 100)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 0.00630802, 3.45, 0, 3447280)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Rotating")
   }
 
   "it" should "calculate rig state and return Rotating Pumping" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 120, 45, 0, 508)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 0.00630912, 3.45, 0, 3447480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Rotating Pumping")
   }
 
   "it" should "calculate rig state and return Pumping" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 159, 8, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 10.1, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 10, 123, 0.00630912, 0.98, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Pumping")
   }
 
   "it" should "calculate rig state and return Rotary Drilling" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 121, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 122.8, 123, 159, 68, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 121, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 122.8, 123, 0.00630912, 3.81, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Rotary Drilling")
   }
 
   "it" should "calculate rig state and return Slide Drilling" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 121, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 122.86, 123, 159, 7, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 121, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 122.86, 123, 0.00630912, 0.8, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Slide Drilling")
   }
 
   "it" should "calculate rig state and return Tripping Out Pumping" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 15, 123, 159, 7, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 15, 123, 0.00630912, 0.8, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Tripping Out Pumping")
   }
 
   "it" should "calculate rig state and return Tripping Out" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 15, 123, 59, 7, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 15, 123, 0.00630812, 0.8, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Tripping Out")
   }
 
   "it" should "calculate rig state and return Tripping Out Rotating" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 15, 123, 59, 87, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 15, 123, 0.00630812, 4.87, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Tripping Out Rotating")
   }
 
   "it" should "calculate rig state and return Tripping In Pumping" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 159, 8, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 0.00631812, 0.68, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Tripping In Pumping")
   }
 
   "it" should "calculate rig state and return Tripping In" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 19, 8, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 0.00630012, 0.48, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Tripping In")
   }
 
   "it" should "calculate rig state and return Tripping In Rotating" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 19, 88, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 0.00630702, 4.5, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Tripping In Rotating")
   }
 
   "it" should "calculate rig state and return In Slips" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 19, 88, 1, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 22, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 0.00630702, 4.5, 1, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "In Slips")
   }
 
   "it" should "calculate rig state and return Back Ream" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 102, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 129, 88, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 102, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 85, 123, 0.00631902, 4.5, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Back Ream")
   }
 
   "it" should "calculate rig state and return Ream In" in {
-    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 102, 123, 80, 45, 0, 100)
-    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 112, 123, 129, 88, 0, 527)
+    val previousRigStateData = RigStateData("Rig State 123", 1539926210700L, 102, 123, 0.00630802, 1.00, 0, 3447280)
+    val currentRigStateData = RigStateData("Rig State 123", 1539926210702L, 112, 123, 0.00631902, 4.5, 0, 3448480)
     assert(RigStateCalculator.calculateRigState(previousRigStateData, currentRigStateData) == "Ream In")
   }
 }
